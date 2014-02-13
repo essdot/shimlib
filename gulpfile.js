b17974fa8370384d89e0e06dea56789a95c7d690
essdot <jsippel@fool.com>
2014-02-12 19:34:25 -0800

    Clean up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,24 +7,30 @@ var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 
+var paths = {
+	outputDir: 'min',
+	shimlibSpecs: [ 'test/shimlib-spec/*.js', 'test/shimlib-integration-spec/*.js' ],
+	shimlibModule: 'app/shimlib.js',
+};
+
 gulp.task('clean', function() {
-	gulp.src('./min', { read: false })
+	gulp.src(paths.outputDir, { read: false })
 		.pipe(clean());
 });
 
 gulp.task('prepare-spec', function() {
-	gulp.src( [ 'test/shimlib-spec/*.js', 'test/shimlib-integration-spec/*.js' ])
+	gulp.src(paths.shimlibSpecs)
 		.pipe(concat('shimlib-spec.browserified.js'))
 		.pipe(browserify())
-		.pipe(gulp.dest('./min'));
+		.pipe(gulp.dest(paths.outputDir));
 });
 
 gulp.task('prepare-shimlib-standalone', function() {
-	gulp.src('app/shimlib.js')
+	gulp.src(paths.shimlibModule)
 		.pipe(browserify({ standalone: 'shimlib' }))
 		.pipe(uglify())
 		.pipe(rename('shimlib.min.js'))
-		.pipe(gulp.dest('./min'));
+		.pipe(gulp.dest(paths.outputDir));
 });
 
 gulp.task('build', [ 'clean', 'prepare-shimlib-standalone', 'prepare-spec' ]);
